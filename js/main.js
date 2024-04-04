6bacac8fad22b4ba4ace24fc59ef3a7a639abfe2
MD. RONE PAYKER <135639998+rone-payker@users.noreply.github.com>
2024-04-04 21:28:46 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+function loading(){
+    document.getElementById('preloader').style.display = 'none';
+} 
+
+
+$(document).ready(function(){
+  $('#second').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+  }),
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+  })
+});
+
+
